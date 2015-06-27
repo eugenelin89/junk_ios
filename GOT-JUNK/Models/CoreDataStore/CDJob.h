537366549bfd1e793d4e5771418c2297b88fe86a
Eugene Lin <eugenelin@admin-locals-MacBook-Pro.local>
2015-06-26 17:37:19 -0700

    re-generating the source code for added properties for the CDJob class

--- GOT-JUNK/Models/CoreDataStore/CDJob.h
+++ GOT-JUNK/Models/CoreDataStore/CDJob.h
@@ -2,7 +2,7 @@
 //  CDJob.h
 //  GOT-JUNK
 //
-//  Created by Eugene Lin on 2015-06-23.
+//  Created by Eugene Lin on 2015-06-26.
 //  Copyright (c) 2015 David Block. All rights reserved.
 //
 
@@ -13,9 +13,80 @@
 
 @interface CDJob : NSManagedObject
 
-@property (nonatomic, retain) NSNumber * jobID;
-@property (nonatomic, retain) NSDate * jobDate;
+@property (nonatomic, retain) NSString * callAheadStatus;
+@property (nonatomic, retain) NSNumber * callAheadTime;
+@property (nonatomic, retain) NSString * clientCompany;
+@property (nonatomic, retain) NSString * clientEmail;
 @property (nonatomic, retain) NSString * clientName;
+@property (nonatomic, retain) NSNumber * clientTypeID;
+@property (nonatomic, retain) NSString * comments;
+@property (nonatomic, retain) NSString * contactCell;
+@property (nonatomic, retain) NSString * contactCellAreaCode;
+@property (nonatomic, retain) NSString * contactCellExt;
+@property (nonatomic, retain) NSString * contactFax;
+@property (nonatomic, retain) NSString * contactFaxAreaCode;
+@property (nonatomic, retain) NSString * contactFaxExt;
+@property (nonatomic, retain) NSString * contactHomeAreaCode;
+@property (nonatomic, retain) NSString * contactHomeExt;
+@property (nonatomic, retain) NSString * contactHomePhone;
+@property (nonatomic, retain) NSNumber * contactID;
+@property (nonatomic, retain) NSString * contactPagerAreaCode;
+@property (nonatomic, retain) NSString * contactPagerExt;
+@property (nonatomic, retain) NSString * contactPagerPhone;
+@property (nonatomic, retain) NSString * contactPhonePref;
+@property (nonatomic, retain) NSNumber * contactPhonePrefID;
+@property (nonatomic, retain) NSString * contactWorkAreaCode;
+@property (nonatomic, retain) NSString * contactWorkExt;
+@property (nonatomic, retain) NSString * contactWorkPhone;
+@property (nonatomic, retain) NSNumber * discount;
+@property (nonatomic, retain) NSNumber * dispatchAccepted;
+@property (nonatomic, retain) NSNumber * dispatchID;
+@property (nonatomic, retain) NSString * dispatchMessage;
+@property (nonatomic, retain) NSString * invoiceNumber;
+@property (nonatomic, retain) NSNumber * isCashedOut;
+@property (nonatomic, retain) NSNumber * isCentrallyBilled;
+@property (nonatomic, retain) NSNumber * isDispatchAccepted;
+@property (nonatomic, retain) NSNumber * isEnviroRequired;
+@property (nonatomic, retain) NSString * jobComments;
+@property (nonatomic, retain) NSDate * jobDate;
+@property (nonatomic, retain) NSNumber * jobDuration;
+@property (nonatomic, retain) NSString * jobEndTime;
+@property (nonatomic, retain) NSNumber * jobID;
+@property (nonatomic, retain) NSString * jobStartTime;
+@property (nonatomic, retain) NSNumber * jobStartTimeOriginal;
+@property (nonatomic, retain) NSNumber * jobType;
+@property (nonatomic, retain) NSNumber * junkCharge;
+@property (nonatomic, retain) NSString * junkLocationComments;
+@property (nonatomic, retain) NSString * nameOfLastTTUsed;
+@property (nonatomic, retain) NSString * npsComment;
+@property (nonatomic, retain) NSNumber * npsValue;
+@property (nonatomic, retain) NSNumber * numOfJobs;
+@property (nonatomic, retain) NSString * onSiteContactAreaCode;
+@property (nonatomic, retain) NSString * onSiteContactExt;
+@property (nonatomic, retain) NSNumber * onSiteContactID;
+@property (nonatomic, retain) NSString * onSiteContactPhone;
+@property (nonatomic, retain) NSString * onSiteContactPhonePref;
+@property (nonatomic, retain) NSNumber * onSiteContactPhonePrefID;
+@property (nonatomic, retain) NSNumber * paymentID;
+@property (nonatomic, retain) NSString * pickupAddress;
+@property (nonatomic, retain) NSString * pickupCompany;
+@property (nonatomic, retain) NSString * pickupCountry;
+@property (nonatomic, retain) NSNumber * programDiscount;
+@property (nonatomic, retain) NSString * programDiscountType;
+@property (nonatomic, retain) NSString * programNotes;
+@property (nonatomic, retain) NSString * promiseTime;
+@property (nonatomic, retain) NSString * promoCode;
+@property (nonatomic, retain) NSNumber * subTotal;
+@property (nonatomic, retain) NSNumber * taxAmount;
+@property (nonatomic, retain) NSNumber * taxID;
+@property (nonatomic, retain) NSString * taxType;
+@property (nonatomic, retain) NSNumber * total;
+@property (nonatomic, retain) NSNumber * totalSpent;
+@property (nonatomic, retain) NSNumber * typeID;
+@property (nonatomic, retain) NSString * zipCode;
+@property (nonatomic, retain) NSString * zoneColor;
+@property (nonatomic, retain) NSString * zoneFontColor;
+@property (nonatomic, retain) NSString * zoneName;
 @property (nonatomic, retain) CDRoute *route;
 
 @end
--- GOT-JUNK/Models/CoreDataStore/CDJob.m
+++ GOT-JUNK/Models/CoreDataStore/CDJob.m
@@ -2,7 +2,7 @@
 //  CDJob.m
 //  GOT-JUNK
 //
-//  Created by Eugene Lin on 2015-06-23.
+//  Created by Eugene Lin on 2015-06-26.
 //  Copyright (c) 2015 David Block. All rights reserved.
 //
 
@@ -12,9 +12,80 @@
 
 @implementation CDJob
 
-@dynamic jobID;
-@dynamic jobDate;
+@dynamic callAheadStatus;
+@dynamic callAheadTime;
+@dynamic clientCompany;
+@dynamic clientEmail;
 @dynamic clientName;
+@dynamic clientTypeID;
+@dynamic comments;
+@dynamic contactCell;
+@dynamic contactCellAreaCode;
+@dynamic contactCellExt;
+@dynamic contactFax;
+@dynamic contactFaxAreaCode;
+@dynamic contactFaxExt;
+@dynamic contactHomeAreaCode;
+@dynamic contactHomeExt;
+@dynamic contactHomePhone;
+@dynamic contactID;
+@dynamic contactPagerAreaCode;
+@dynamic contactPagerExt;
+@dynamic contactPagerPhone;
+@dynamic contactPhonePref;
+@dynamic contactPhonePrefID;
+@dynamic contactWorkAreaCode;
+@dynamic contactWorkExt;
+@dynamic contactWorkPhone;
+@dynamic discount;
+@dynamic dispatchAccepted;
+@dynamic dispatchID;
+@dynamic dispatchMessage;
+@dynamic invoiceNumber;
+@dynamic isCashedOut;
+@dynamic isCentrallyBilled;
+@dynamic isDispatchAccepted;
+@dynamic isEnviroRequired;
+@dynamic jobComments;
+@dynamic jobDate;
+@dynamic jobDuration;
+@dynamic jobEndTime;
+@dynamic jobID;
+@dynamic jobStartTime;
+@dynamic jobStartTimeOriginal;
+@dynamic jobType;
+@dynamic junkCharge;
+@dynamic junkLocationComments;
+@dynamic nameOfLastTTUsed;
+@dynamic npsComment;
+@dynamic npsValue;
+@dynamic numOfJobs;
+@dynamic onSiteContactAreaCode;
+@dynamic onSiteContactExt;
+@dynamic onSiteContactID;
+@dynamic onSiteContactPhone;
+@dynamic onSiteContactPhonePref;
+@dynamic onSiteContactPhonePrefID;
+@dynamic paymentID;
+@dynamic pickupAddress;
+@dynamic pickupCompany;
+@dynamic pickupCountry;
+@dynamic programDiscount;
+@dynamic programDiscountType;
+@dynamic programNotes;
+@dynamic promiseTime;
+@dynamic promoCode;
+@dynamic subTotal;
+@dynamic taxAmount;
+@dynamic taxID;
+@dynamic taxType;
+@dynamic total;
+@dynamic totalSpent;
+@dynamic typeID;
+@dynamic zipCode;
+@dynamic zoneColor;
+@dynamic zoneFontColor;
+@dynamic zoneName;
 @dynamic route;
 
 @end
